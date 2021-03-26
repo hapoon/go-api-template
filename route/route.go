824d52041a9435670dafb841ea20c761c90312a5
hapoon <hapoon24@gmail.com>
2021-03-26 15:26:09 +0900

    Add API Version.

--- route/route.go
+++ route/route.go
@@ -1,36 +1,14 @@
 package route
 
 import (
-	"hapoon/go-api-template/service"
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func Route(e *echo.Echo) {
-	// /sample
-	sample := service.NewSampleService()
-	s := e.Group("/sample")
-	{
-		s.GET("/", func(c echo.Context) error {
-			res, _ := sample.ListSamples()
-			return c.JSON(http.StatusOK, res)
-		})
-		s.GET("/:id", func(c echo.Context) error {
-			id := c.Param("id")
-			res, _ := sample.GetSample(id)
-			return c.JSON(http.StatusOK, res)
-		})
-		s.POST("/", func(c echo.Context) error {
-			var body = struct {
-				Id   string `json:"id"`
-				Name string `json:"name"`
-			}{}
-			if err := c.Bind(&body); err != nil {
-				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-			}
-			return c.JSON(http.StatusAccepted, body)
-		})
-	}
-
+	// API v1
+	ApiV1(e)
+	// API v2
+	// APIのVersionを追加する場合はvX.goの形式でファイルを新規作成して
+	// 関数を呼び出すようにしてください
+	// ApiV2(e)
 }
